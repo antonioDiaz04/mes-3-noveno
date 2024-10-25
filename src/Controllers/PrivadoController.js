6748ffa95055cf223ac067502fcdff750058b288
Eduardo_465 <20221076@uthh.edu.mx>
2024-10-25 07:49:27 -0600

    crud

--- src/Controllers/PrivadoController.js
+++ src/Controllers/PrivadoController.js
@@ -1,6 +1,7 @@
-const { Politicas } = require("../Models/PrivadoModel");
+const { Politicas, TerminosYCondiciones,Deslindelegal  } = require("../Models/PrivadoModel");
 const { AcercaDe, Contacto, Pregunta } = require("../Models/PrivadoModel.js");
 
+// Acerca de
 exports.createAcercaDe = async (req, res) => {
   try {
     const { titulo, contenido } = req.body;
@@ -64,10 +65,14 @@ exports.updateAcercaDe = async (req, res) => {
   }
 };
 
-// Crear nuevas políticas o actualizar la existente
+//políticas
 exports.crearPoliticas = async (req, res) => {
   try {
-    const { titulo, contenido } = req.body;
+    const { titulo, contenido, fechaVigencia } = req.body;
+
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
 
     const nuevaVersion = "1.0";
 
@@ -77,6 +82,7 @@ exports.crearPoliticas = async (req, res) => {
       contenido: contenido,
       version: nuevaVersion,
       estado: "vigente",
+      fechaVigencia: fechaVigencia,
       historial: [],
     });
 
@@ -93,32 +99,52 @@ exports.crearPoliticas = async (req, res) => {
   }
 };
 
-// Obtener todas las políticas
 exports.obtenerPoliticas = async (req, res) => {
   try {
     const politicas = await Politicas.find({ estado: { $ne: "eliminado" } });
+
+    if (!politicas) {
+      return res.status(404).json({ message: "No hay políticas disponibles" });
+    }
+    const fechaHoy = new Date();
+
+    // Actualizar el estado de las políticas que han pasado su fecha de vigencia
+    for (const politica of politicas) {
+      if (
+        new Date(politica.fechaVigencia) < fechaHoy &&
+        politica.estado === "vigente"
+      ) {
+        politica.estado = "no vigente"; // Cambiar estado a "no vigente"
+        await politica.save(); // Guardar los cambios
+      }
+    }
+
     return res.status(200).json(politicas);
   } catch (error) {
     console.log("Error al obtener políticas:", error);
     return res.status(500).send("Error en el servidor: " + error);
   }
 };
-// Actualizar políticas existentes
 exports.actualizarPoliticas = async (req, res) => {
   try {
     const { id } = req.params;
-    const { titulo, contenido } = req.body;
+    const { titulo, contenido, fechaVigencia } = req.body;
 
     const politicaExistente = await Politicas.findById(id);
 
     if (!politicaExistente) {
       return res.status(404).send("Política no encontrada");
     }
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
 
+    // Guardar el historial de la política existente
     politicaExistente.historial.push({
       version: politicaExistente.version,
       titulo: politicaExistente.titulo,
       contenido: politicaExistente.contenido,
+      fechaVigencia: politicaExistente.fechaVigencia,
       estado: "no vigente",
       fechaCreacion: new Date(),
     });
@@ -130,7 +156,14 @@ exports.actualizarPoliticas = async (req, res) => {
     politicaExistente.titulo = titulo;
     politicaExistente.contenido = contenido;
     politicaExistente.version = nuevaVersion;
-    politicaExistente.estado = "vigente"; // Asumiendo que el nuevo contenido es vigente
+    politicaExistente.fechaVigencia = fechaVigencia; // Actualizar la fecha de vigencia
+
+    // Verificar si la nueva fecha de vigencia es válida
+    if (new Date(fechaVigencia) > new Date()) {
+      politicaExistente.estado = "vigente"; // La política es vigente si la fecha es futura
+    } else {
+      politicaExistente.estado = "no vigente"; // La política no es vigente si la fecha ya pasó
+    }
 
     const politicaActualizada = await politicaExistente.save();
 
@@ -144,7 +177,6 @@ exports.actualizarPoliticas = async (req, res) => {
   }
 };
 
-// Eliminar una política específica
 exports.eliminarPolitica = async (req, res) => {
   try {
     const { id } = req.params;
@@ -187,6 +219,7 @@ exports.obtenerHistorialPolitica = async (req, res) => {
         titulo: politica.titulo,
         contenido: politica.contenido,
         estado: politica.estado,
+        fechaVigencia: politica.fechaVigencia,
         fechaCreacion: politica.fechaCreacion, // Asegúrate de tener esta propiedad en tu esquema
       },
       ...politica.historial, // Agregar el historial anterior
@@ -197,6 +230,336 @@ exports.obtenerHistorialPolitica = async (req, res) => {
     });
   } catch (error) {
     console.log("Error al obtener el historial de la política:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+//Terminos y condiciones
+exports.crearTerminosYCondiciones = async (req, res) => {
+  try {
+    const { titulo, contenido, fechaVigencia } = req.body;
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
+
+    const nuevosTerminos = new TerminosYCondiciones({
+      titulo: titulo,
+      contenido: contenido,
+      version: "1.0",
+      estado: "vigente",
+      fechaVigencia: fechaVigencia,
+    });
+
+    await nuevosTerminos.save();
+
+    return res.status(201).json({
+      message: "Términos y condiciones creados exitosamente",
+      terminos: nuevosTerminos,
+    });
+  } catch (error) {
+    console.error("Error al crear términos y condiciones:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+exports.obtenerTerminosYCondiciones = async (req, res) => {
+  try {
+    const terminos = await TerminosYCondiciones.find({
+      estado: { $ne: "eliminado" },
+    });
+    if (!terminos) {
+      return res.status(404).json({ message: "No hay términos y condiciones disponibles" });
+    }
+
+    const fechaHoy = new Date();
+
+    // Actualizar el estado de las políticas que han pasado su fecha de vigencia
+    for (const termino of terminos) {
+      if (
+        new Date(termino.fechaVigencia) < fechaHoy &&
+        termino.estado === "vigente"
+      ) {
+        termino.estado = "no vigente"; // Cambiar estado a "no vigente"
+        await termino.save(); // Guardar los cambios
+      }
+    }
+    return res.status(200).json(terminos);
+  } catch (error) {
+    console.error("Error al obtener términos y condiciones:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+exports.actualizarTerminosYCondiciones = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, contenido, fechaVigencia } = req.body;
+
+    const terminosExistentes = await TerminosYCondiciones.findById(id);
+
+    if (!terminosExistentes) {
+      return res.status(404).send("Términos y condiciones no encontrados");
+    }
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
+    console.log(titulo, contenido, fechaVigencia)
+
+    // Guardar el historial de la versión anterior
+    terminosExistentes.historial.push({
+      version: terminosExistentes.version,
+      titulo: terminosExistentes.titulo,
+      contenido: terminosExistentes.contenido,
+      fechaVigencia: terminosExistentes.fechaVigencia,
+      estado: "no vigente",
+      fechaCreacion: new Date(),
+    });
+
+    // Incrementar la versión
+    const nuevaVersion = (parseFloat(terminosExistentes.version) + 1).toFixed(
+      1
+    );
+
+    // Actualizar los campos de los términos
+    terminosExistentes.titulo = titulo;
+    terminosExistentes.contenido = contenido;
+    terminosExistentes.fechaVigencia = fechaVigencia;
+    terminosExistentes.version = nuevaVersion;
+    terminosExistentes.estado = "vigente"; // Asumiendo que el nuevo contenido es vigente
+
+    const terminosActualizados = await terminosExistentes.save();
+
+    return res.status(200).json({
+      message: "Términos y condiciones actualizados exitosamente",
+      terminos: terminosActualizados,
+    });
+  } catch (error) {
+    console.error("Error al actualizar términos y condiciones:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+exports.eliminarTerminosYCondiciones = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const terminos = await TerminosYCondiciones.findById(id);
+
+    if (!terminos) {
+      return res
+        .status(404)
+        .json({ message: "Términos y condiciones no encontrados" });
+    }
+
+    terminos.estado = "eliminado"; // Cambiar estado a eliminado
+    await terminos.save();
+
+    return res.status(204).json({
+      message: "Términos y condiciones eliminados",
+    });
+  } catch (error) {
+    console.error("Error al eliminar términos y condiciones:", error);
+    return res.status(500).json({
+      mensaje: "Error interno del servidor",
+      error: error.message,
+    });
+  }
+};
+
+exports.obtenerHistorialTerminosYCondiciones = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Buscar los términos por su ID
+    const terminos = await TerminosYCondiciones.findById(id);
+    if (!terminos) {
+      return res.status(404).send("Términos y condiciones no encontrados");
+    }
+
+    const historialCompleto = [
+      {
+        version: terminos.version,
+        titulo: terminos.titulo,
+        contenido: terminos.contenido,
+        fechaVigencia: terminos.fechaVigencia,
+        estado: terminos.estado,
+        fechaCreacion: terminos.fechaCreacion,
+      },
+      ...terminos.historial, // Agregar el historial anterior
+    ];
+
+    return res.status(200).json({
+      historial: historialCompleto,
+    });
+  } catch (error) {
+    console.error(
+      "Error al obtener el historial de términos y condiciones:",
+      error
+    );
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+
+
+exports.crearDeslindeLegal = async (req, res) => {
+  try {
+    const { titulo, contenido, fechaVigencia } = req.body;
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
+
+    const nuevoDeslinde = new Deslindelegal({
+      titulo: titulo,
+      contenido: contenido,
+      version: "1.0",
+      estado: "vigente",
+      fechaVigencia: fechaVigencia,
+    });
+
+    await nuevoDeslinde.save();
+
+    return res.status(201).json({
+      message: "Deslinde legal creado exitosamente",
+      deslinde: nuevoDeslinde,
+    });
+  } catch (error) {
+    console.error("Error al crear deslinde legal:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+// Obtener todos los Deslindes Legales
+exports.obtenerDeslindesLegales = async (req, res) => {
+  try {
+    const deslindes = await Deslindelegal.find({
+      estado: { $ne: "eliminado" },
+    });
+
+    if (!deslindes) {
+      return res.status(404).json({ message: "No hay deslindes legales disponibles" });
+    }
+
+    const fechaHoy = new Date();
+
+    // Actualizar el estado de los deslindes que han pasado su fecha de vigencia
+    for (const deslinde of deslindes) {
+      if (
+        new Date(deslinde.fechaVigencia) < fechaHoy &&
+        deslinde.estado === "vigente"
+      ) {
+        deslinde.estado = "no vigente";
+        await deslinde.save();
+      }
+    }
+
+    return res.status(200).json(deslindes);
+  } catch (error) {
+    console.error("Error al obtener deslindes legales:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+// Actualizar Deslinde Legal
+exports.actualizarDeslindeLegal = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, contenido, fechaVigencia } = req.body;
+
+    const deslindeExistente = await Deslindelegal.findById(id);
+
+    if (!deslindeExistente) {
+      return res.status(404).send("Deslinde legal no encontrado");
+    }
+    if (!fechaVigencia || isNaN(new Date(fechaVigencia).getTime())) {
+      return res.status(400).json({ message: "Fecha de vigencia inválida" });
+    }
+
+    // Guardar el historial de la versión anterior
+    deslindeExistente.historial.push({
+      version: deslindeExistente.version,
+      titulo: deslindeExistente.titulo,
+      contenido: deslindeExistente.contenido,
+      fechaVigencia: deslindeExistente.fechaVigencia,
+      estado: "no vigente",
+      fechaCreacion: new Date(),
+    });
+
+    // Incrementar la versión
+    const nuevaVersion = (parseFloat(deslindeExistente.version) + 1).toFixed(1);
+
+    // Actualizar los campos del deslinde
+    deslindeExistente.titulo = titulo;
+    deslindeExistente.contenido = contenido;
+    deslindeExistente.fechaVigencia = fechaVigencia;
+    deslindeExistente.version = nuevaVersion;
+    deslindeExistente.estado = "vigente";
+
+    const deslindeActualizado = await deslindeExistente.save();
+
+    return res.status(200).json({
+      message: "Deslinde legal actualizado exitosamente",
+      deslinde: deslindeActualizado,
+    });
+  } catch (error) {
+    console.error("Error al actualizar deslinde legal:", error);
+    return res.status(500).send("Error en el servidor: " + error);
+  }
+};
+
+// Eliminar Deslinde Legal (lógica)
+exports.eliminarDeslindeLegal = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deslinde = await Deslindelegal.findById(id);
+
+    if (!deslinde) {
+      return res.status(404).json({ message: "Deslinde legal no encontrado" });
+    }
+
+    deslinde.estado = "eliminado";
+    await deslinde.save();
+
+    return res.status(204).json({
+      message: "Deslinde legal eliminado",
+    });
+  } catch (error) {
+    console.error("Error al eliminar deslinde legal:", error);
+    return res.status(500).json({
+      mensaje: "Error interno del servidor",
+      error: error.message,
+    });
+  }
+};
+
+// Obtener Historial del Deslinde Legal
+exports.obtenerHistorialDeslindeLegal = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deslinde = await Deslindelegal  .findById(id);
+    if (!deslinde) {
+      return res.status(404).send("Deslinde legal no encontrado");
+    }
+
+    const historialCompleto = [
+      {
+        version: deslinde.version,
+        titulo: deslinde.titulo,
+        contenido: deslinde.contenido,
+        fechaVigencia: deslinde.fechaVigencia,
+        estado: deslinde.estado,
+        fechaCreacion: deslinde.fechaCreacion,
+      },
+      ...deslinde.historial,
+    ];
+
+    return res.status(200).json({
+      historial: historialCompleto,
+    });
+  } catch (error) {
+    console.error("Error al obtener el historial del deslinde legal:", error);
     return res.status(500).send("Error en el servidor: " + error);
   }
 };
--- src/Routes/PrivadoRoute.js
+++ src/Routes/PrivadoRoute.js
@@ -9,11 +9,49 @@ const {
   obtenerHistorialPolitica,
 } = require("../Controllers/PrivadoController");
 
-router.post("/crearPoliticas", crearPoliticas);
+const {
+  crearTerminosYCondiciones,
+  actualizarTerminosYCondiciones,
+  obtenerTerminosYCondiciones,
+  eliminarTerminosYCondiciones,
+  obtenerHistorialTerminosYCondiciones,
+} = require("../Controllers/PrivadoController");
+const {
+crearDeslindeLegal,actualizarDeslindeLegal,eliminarDeslindeLegal,obtenerDeslindesLegales,obtenerHistorialDeslindeLegal
+} = require("../Controllers/PrivadoController");
+
 router.get("/obtenerPoliticas", obtenerPoliticas);
+router.get("/obtenerTerminosYCondiciones", obtenerTerminosYCondiciones);
+router.get(
+  "/obtenerHistorialTerminosYCondiciones/:id",
+  obtenerHistorialTerminosYCondiciones
+);
 router.get("/obtenerHistorialPoliticas/:id", obtenerHistorialPolitica);
+
+router.post("/crearPoliticas", crearPoliticas);
+
+router.post("/crearTerminosYCondiciones", crearTerminosYCondiciones);
 router.put("/actualizarPoliticas/:id", actualizarPoliticas);
-// Eliminar un perfil de empresa
+
+router.put(
+  "/actualizarTerminosYCondiciones/:id",
+  actualizarTerminosYCondiciones
+);
 router.delete("/eliminarPolitica/:id", eliminarPolitica);
+
+
+
+router.delete(
+  "/eliminarTerminosYCondiciones/:id",
+  eliminarTerminosYCondiciones
+);
+
+
+// Rutas para Deslinde Legal
+router.get("/obtenerDeslindesLegales", obtenerDeslindesLegales);
+router.post("/crearDeslindeLegal", crearDeslindeLegal);
+router.put("/actualizarDeslindeLegal/:id", actualizarDeslindeLegal);
+router.delete("/eliminarDeslindeLegal/:id", eliminarDeslindeLegal);
+router.get("/obtenerHistorialDeslindeLegal/:id", obtenerHistorialDeslindeLegal);
 
 module.exports = router;
