cd3716224a1f4e318330b8d88c9343ddc9422530
Antonius <134122438+NTO-H@users.noreply.github.com>
2024-11-26 16:03:04 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const corsOptions = {
   origin: [
     "https://proyecto-atr.vercel.app", // URL de tu frontend en producción
     "http://localhost:4200",
-    "https://proyectoatr.com/"// URL local para desarrollo
+    "https://proyectoatr.com"// URL local para desarrollo
     // "https://nhnwlf08-4200.usw3.devtunnels.ms/" // URL local para desarrollo
   ],
   // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
