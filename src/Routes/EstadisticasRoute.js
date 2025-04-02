dcfabd80782cc650674b5f0ce524cc99c811ca56
antonioDiaz04 <20221136@uthh.edu.mx>
2025-04-02 09:55:34 -0600

    comment estdisit

--- src/Routes/EstadisticasRoute.js
+++ src/Routes/EstadisticasRoute.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const {
-  obtenerClientesFrecuentes,
-  obtenerClientesActivos,
-  obtenerGastoPromedio,
-  obtenerIngresosPorMes,
-  obtenerIngresosTotales,
-  obtenerMetodosPago,
-  obtenerProductosPopulares,
-  obtenerRentasRetrasadas,
-  obtenerTotales,
-} = require("../Controllers/EstadisticasController.js");
+// const express = require("express");
+// const {
+//   obtenerClientesFrecuentes,
+//   obtenerClientesActivos,
+//   obtenerGastoPromedio,
+//   obtenerIngresosPorMes,
+//   obtenerIngresosTotales,
+//   obtenerMetodosPago,
+//   obtenerProductosPopulares,
+//   obtenerRentasRetrasadas,
+//   obtenerTotales,
+// } = require("../Controllers/EstadisticasController.js");
 
-const router = express.Router();
+// const router = express.Router();
 
-// router.get("/", obtenerEstadisticas);
-router.get("/totales", obtenerTotales);
-router.get("/ingresos", obtenerIngresosTotales);
-router.get("/productos-populares", obtenerProductosPopulares);
-router.get("/clientes-activos", obtenerClientesActivos);
-router.get("/ingresos-mensuales", obtenerIngresosPorMes);
-router.get("/metodos-pago", obtenerMetodosPago);
-router.get("/rentas-retrasadas", obtenerRentasRetrasadas);
-router.get("/clientes-frecuentes", obtenerClientesFrecuentes);
-router.get("/gasto-promedio", obtenerGastoPromedio);
+// // router.get("/", obtenerEstadisticas);
+// router.get("/totales", obtenerTotales);
+// router.get("/ingresos", obtenerIngresosTotales);
+// router.get("/productos-populares", obtenerProductosPopulares);
+// router.get("/clientes-activos", obtenerClientesActivos);
+// router.get("/ingresos-mensuales", obtenerIngresosPorMes);
+// router.get("/metodos-pago", obtenerMetodosPago);
+// router.get("/rentas-retrasadas", obtenerRentasRetrasadas);
+// router.get("/clientes-frecuentes", obtenerClientesFrecuentes);
+// router.get("/gasto-promedio", obtenerGastoPromedio);
 
-module.exports = router;
+// module.exports = router;
