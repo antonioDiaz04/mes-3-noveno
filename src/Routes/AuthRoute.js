9fa29b76fac7e634ca7696757764deb7b6bae0a7
eduardocastro465 <152236610+eduardocastro465@users.noreply.github.com>
2025-04-03 09:07:34 -0600

    Update AuthRoute.js

--- src/Routes/AuthRoute.js
+++ src/Routes/AuthRoute.js
@@ -8,6 +8,7 @@ const {loginSchema} = require("../schemas/authSchema");
 
 // router.post("/signIn", validarDatos(loginSchema), AutController.Login);
 router.post("/signIn", AutController.Login);
+router.post("/signIn-Google-Facebook", AutController.signInGoogleFacebook);
 // router.post("/enviar-confirmar", AutController.enviarConfirmar);
 //autentificacion/signIn-Google-Facebook
 // router.post("/verficar-codigo", AutController.verificarCodigo);
